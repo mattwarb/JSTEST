340bee0e749c26c666da7f5204908c2f9d02712e
mattjwarburton@gmail.com <Matt>
2018-04-20 13:15:22 +0100

    got the test

---
+++ assessment.js
@@ -0,0 +1,20 @@
+
+function isPrime(number){
+
+}
+  
+function amountToCoins(amount, coins){
+
+}
+
+function uniqueLetters(string){
+
+}
+
+function factorial(number){
+
+}
+
+function fibonacci(number){
+
+}
